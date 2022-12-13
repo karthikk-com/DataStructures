f95cb894c55af943da4b29d63d4e6939de329f6f
karthikk-com <karthik@karthikk.com>
2022-12-13 19:00:13 +0530

    Datastructure:HashTables -> Implementation using Arrays - (updated - Key() method)

--- HashTable/HashTableImplementation.js
+++ HashTable/HashTableImplementation.js
@@ -15,7 +15,7 @@ class HashTable {
     }
 
     /* Returns the respective value of the key input passed*/
-    get(key){
+    get(key) {
         const address = this._hashKey(key);
         const value = this.data[address];
 
@@ -31,11 +31,29 @@ class HashTable {
         }
     }
 
+    /* Retrieve all the keys from the hash tables */
+    key() {
+        let keys = [];
+        console.log(this.data);
+        for(let i = 0; i < this.data.length; i++) {
+            if(this.data[i]) {
+                if(this.data[i].length > 1){ //INFO : If collision then Big O -> O(n^2)
+                    for(let j = 0; j < this.data[i].length; j++) {
+                        keys.push(this.data[i][j][0]);
+                    }
+                } else {//INFO : If no collision then Big O -> O(n)
+                    keys.push(this.data[i][0][0]);
+                }
+            }
+        }
+        return keys;
+    }
+
     /* Simulating a hash function that returns a hash value based on the inuput string
     Note : The naming convention followed in javascript community starting with "_" (underscore), 
     represent that this is a private function that shouldn't be accessed outside the class. 
     */
-    _hashKey(key){
+    _hashKey(key) {
         let hash = 0;
         for(let i = 0; i < key.length; i++){
             hash = (hash + key.charCodeAt(i) * i) % this.data.length;
@@ -44,7 +62,7 @@ class HashTable {
     }
 }
 
-const myHashtable = new HashTable(50);
+const myHashtable = new HashTable(2);
 console.log(myHashtable);
 console.log(myHashtable.set('grapes',1000)); //[ <23 empty items>, [ [ 'grapes', 1000 ] ], <26 empty items> ]
 console.log(myHashtable.set('Oranges',2000)); //[ <23 empty items>,[ [ 'grapes', 1000 ] ],<21 empty items>,[ [ 'Oranges', 2000 ] ],<4 empty items>]
@@ -53,3 +71,5 @@ console.log(myHashtable.set('apples',3000)); //[<23 empty items>,[ [ 'grapes', 1
 console.log(myHashtable.get('Oranges')); //2000
 console.log(myHashtable.get('apples')); //3000
 console.log(myHashtable.get('vegetable')); //undefined
+
+console.log(myHashtable.key());
